032ff2ddf248d50c5904c275e4f7278af3794859
Naturalium <tlawodud1995@gmail.com>
2020-10-28 06:16:53 +0000

    2020_10_28

--- git_repo/linux_ex/ex08/ex08-12.c
+++ git_repo/linux_ex/ex08/ex08-12.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-  char *envlist[] = {"APPLE=0", "BANANA=0", (char *)0};
+  char *envlist[] = {"APPLE=0", "BANANA=1", (char *)0};
 
   execle("ex08-13", "ex08-13", (char *) 0, envlist);
 }
